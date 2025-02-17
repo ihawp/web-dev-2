e537f2c71456866574a7a9b9b78aa8a499f81a8c
ihawp <ihawpebay@gmail.com>
2025-02-17 15:36:43 -0600

    Significant updates made to the geolocation presentation, now a working prototype of a google maps style app exists! There is a target location and if you reach that location the app will tell you! While also showing you a live map of where you are in the world. (Could implement some sort of navigation I suppose?) Since we know what way they are moving (when they move), like go north to get there and then have compass or like arrow that shows you the way... cool stuff.

--- js/geolocation-example.js
+++ js/geolocation-example.js
@@ -1,19 +1,77 @@
+// DOM gets
+const geoLocation = document.getElementById("geolocation");
+const alert = document.getElementById('alert');
+
 /*
 
-    ADD COMMENTS TO OUTLINE FOR PRESENTATION
+    Options
+    The api intakes these options to determine how it should act.
+
+    enableHighAccuracy: true;
+    Causes more power consumption and slower response time, but
+    only if the device is able to find a more accurate position.
+
+    maximumAge:
+        The maximum age of a cached location allowed is the value set to maximumAge.
+        If unset (default: 0) the app will always require a new location.
+        If set to Infinity then the device must find a cached location.
+
 
 */
 const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
+    maximumAge: 0,
+    timeout: Infinity,
+    enableHighAccuracy: true
 };
+
+/*
+
+    Initialize variable (id) to hold id of watchPosition() for clearWatch().
+
+ */
+const id = navigator.geolocation.watchPosition(success, error, options);
+
 function success(pos) {
-    document.getElementById("geolocation").src = `https://www.openstreetmap.org/export/embed.html?bbox
-=${pos.coords.longitude-0.01},${pos.coords.latitude-0.01},${pos.coords.longitude+0.01},${pos.coords.latitude+0.01}&layer=mapnik&marker=${pos.coords.latitude},${pos.coords.longitude}`;
+
+    /*
+
+        Upon success the longitude/latitude position of the user is passed
+        through callback to the success callback (with has one parameter, the
+        users returned position)
+
+     */
+    const userLocation = {
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude
+    }
+
+    const targetLocation = {
+    }
+
+    // Add map to 'src' of iframe on DOM
+    const openStreetMapURL = `https://www.openstreetmap.org/export/embed.html?bbox=${ userLocation.longitude },${ userLocation.latitude },${ userLocation.longitude },${ userLocation.latitude }&layer=mapnik&marker=${ userLocation.latitude },${ userLocation.longitude }`;
+
+    if (geoLocation.src !== openStreetMapURL) {
+        geoLocation.src = openStreetMapURL;
+    }
+
+    // Determine if you reached the target location!
+    if (targetLocation.latitude >= userLocation.latitude - 0.001 && targetLocation.latitude <= userLocation.latitude + 0.001 && targetLocation.longitude >= userLocation.longitude - 0.001 && targetLocation.longitude <= userLocation.longitude + 0.001) {
+        navigator.clearWatch(id);
+        alert.innerText = 'You have reached the target location!';
+    } else {
+        alert.innerText = 'You are NOT at the target location.'
+    }
 }
 
 function error(err) {
     console.warn(`Error (${err.code}): ${err.message}`);
 }
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+
+/*
+
+    Get the position of the user and stop.
+
+ */
+// navigator.geolocation.getCurrentPosition(success, error, options);
