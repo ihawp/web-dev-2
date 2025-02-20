a35a6773c5fd5feed31aa3cf4aa69010a95dc425
ihawp <ihawpebay@gmail.com>
2025-02-19 20:37:31 -0600

    Read about async functions and why forEach might not work so well. Have switch to forEach for now, dont see a point in mapping.. yet? Could probably swap to (for in) though. Working well to get the closest street over and over... although it just feels like so much processing. The real issue (in terms of the over processing) is the return of multiple, or chance of return, of multiple lat/lon points from the node inquired upon, which means you have to parse and compare all the returned lat/lon values to guarantee the closest street (highway). Perhaps I should read deeper into the Overpass API documentation, perhaps there is a query limit, but a limit is not really what I mean as that could limit to a lat/lon position in Europe. Doesnt exist, great! I must be on the right track. Going to continue working, but liking where this is at. Need a better main() function.

--- js/geolocation-example-trial.js
+++ js/geolocation-example-trial.js
@@ -5,6 +5,8 @@
 const locationFinder = {
     route: [],
     lowest: {},
+    looking: false,
+    really: false,
     options: {
         maximumAge: 0,
         timeout: Infinity,
@@ -14,6 +16,8 @@ const locationFinder = {
         navigator.geolocation.getCurrentPosition(this.success, this.error, this.options)
     },
     success: function(position) {
+
+        locationFinder.looking = true;
 
         console.log('success');
         locationFinder.latitude = position.coords.latitude;
@@ -27,8 +31,6 @@ const locationFinder = {
             .bindPopup('your location')
             .openPopup();
 
-        console.log(locationFinder.latitude, locationFinder.longitude);
-
         locationFinder.latSign = Math.sign(position.coords.latitude);
         locationFinder.lonSign = Math.sign(position.coords.longitude);
         fetcher(`[out:json]; way(around:50, ${locationFinder.latitude}, ${locationFinder.longitude})["highway"]; out body; `)
@@ -40,43 +42,51 @@ const locationFinder = {
 
                  */
 
+                let l = data.elements;
 
-                data.elements.forEach((item, key) => {
-
+                for (let i = 0; i < l.length; i++) {
                     // Query to get the lat/lon values from node
 
-                    fetcher(`[out:json]; node(id: ${item.id}, ${item.nodes}); out body;`)
-                        .then(dataa => {
+                    fetcher(`[out:json]; node(id: ${l[i].id}, ${l[i].nodes}); out body;`)
+                        .then(data => {
 
                             // Returned value is multiple lat/lons
 
-                            dataa.elements.forEach((itemm, keyy) => {
+                            let q = data.elements;
+
+                            for (let g = 0; g < q.length; g++) {
 
                                 // compare with current user position (of this run)
 
-                                const lati = locationFinder.latitude - itemm.lat;
-                                const longi = locationFinder.longitude - itemm.lon;
+                                const lati = locationFinder.latitude - q[g].lat;
+                                const longi = locationFinder.longitude - q[g].lon;
 
-                                if (keyy === 0) {
+                                if (g === 0) {
                                     locationFinder.lowest.latVal = lati;
                                     locationFinder.lowest.lonVal = longi;
                                 }
 
-                                if (locationFinder.latSign === Math.sign(itemm.lat) && locationFinder.lonSign === Math.sign(itemm.lon)) {
+                                if (locationFinder.latSign === Math.sign(q[g].lat) && locationFinder.lonSign === Math.sign(q[g].lon)) {
                                     // allowed to go.
                                     if (Math.abs(locationFinder.lowest.latVal) > Math.abs(lati) && Math.abs(locationFinder.lowest.lonVal) > Math.abs(longi)) {
                                         locationFinder.lowest.latVal = lati;
                                         locationFinder.lowest.lonVal = longi;
-                                        locationFinder.lowest.latitude = itemm.lat;
-                                        locationFinder.lowest.longitude = itemm.lon;
+                                        locationFinder.lowest.latitude = q[g].lat;
+                                        locationFinder.lowest.longitude = q[g].lon;
                                         console.log(locationFinder.lowest);
-                                        console.log(keyy, dataa.elements);
+                                    }
+                                    if (i === l.length - 1 && g === q.length - 1) {
+                                        locationFinder.really = true;
+                                        locationFinder.looking = false;
+                                        L.marker([locationFinder.lowest.latitude, locationFinder.lowest.longitude]).addTo(map)
+                                            .bindPopup('route 1')
+                                            .openPopup();
+
                                     }
                                 }
-                            })
-                        })
-                });
-                console.log(locationFinder.lowest);
+                            }
+                        });
+                }
             })
     },
     error: function(error) {
@@ -84,12 +94,32 @@ const locationFinder = {
     }
 }
 
-locationFinder.currentPosition();
-
-
 const queryString = `https://overpass-api.de/api/interpreter?data=`;
 async function fetcher(query) {
     return await fetch(queryString + encodeURIComponent(query))
         .then(response => response.json())
         .catch(error => console.error(error));
 }
+
+
+const main = async () => {
+
+    // The reason for not using watchPosition here is because it's too quick! for the overpass API.
+    // Couldn't find a way to guarantee it happening every couple of seconds through options.
+    // So below is a working system, that might be overcomplicated at this point, but will be simplified once I realize.
+
+    let routeFound = false;
+    while (!routeFound) {
+        if (!locationFinder.looking) {
+            locationFinder.currentPosition();
+        }
+        if (locationFinder.really) {
+            locationFinder.really = false;
+            routeFound = true;
+        }
+        routeFound = true;
+    }
+}
+main()
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
