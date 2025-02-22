ace97f1344fddf649e3c7d5971faa34ff2cabd3e
ihawp <ihawpebay@gmail.com>
2025-02-22 12:49:02 -0600

    commenting, all things that are in place work properly, whether they are proper or not will be determined by what is included in the final product :)

--- js/geolocation-3.js
+++ js/geolocation-3.js
@@ -62,9 +62,15 @@ const addMarker = (lat, lon, label) => {
         .openPopup();
 }
 
-// add app.
+// need to add function for finding intersection (in correct direction for a given street),
+// will check documentation what I should be saving for that search
+function findIntersection() {
 
-// add setTimeout to run this (from start())? it will use the modulated functions in the most efficient way for route searching.
+}
+
+// add setTimeout to run this (from start())?,
+// making sure to calculate one new point at a time, checking for an updated user position (for visual reasons),
+// it will use the modulated functions in the most efficient way for route searching.
 // as start is simply to get location and makle the map show you where you're at and where you're going.
 // and then however fast the route is calculated it will appear!
 async function run() {
@@ -75,6 +81,8 @@ async function start() {
 
 
 
+    // get and set user location (set to 'dataStruct' object properties)
+    // add a marker to the map showing the user where they are.
     let userLocation = await getLocation();
     dataStruct.startLat = userLocation.coords.latitude;
     dataStruct.startLon = userLocation.coords.longitude;
@@ -83,28 +91,53 @@ async function start() {
     // figure out quadrant that target is located in (in) relation to the start location.
     // might help in determining if we are going the right direction.
     /*
-    target(-, +)  |     target(+, +)
-                  |
-    ------------start-------------
-                  |
-    target(-, -)  |     target(+, -)
+            target(-, +)  |     target(+, +)
+                          |
+            ------------start-------------
+                          |
+            target(-, -)  |     target(+, -)
      */
 
     dataStruct.targetLat = dataStruct.startLat + 0.0009;
     dataStruct.targetLon = dataStruct.startLon + 0.0009;
     addMarker(dataStruct.targetLat, dataStruct.targetLon, 'Target Location');
 
-    let polyline = L.polyline([[dataStruct.startLat, dataStruct.startLon], [dataStruct.targetLat, dataStruct.targetLon]], {color: 'red'}).addTo(map);
-    map.fitBounds(polyline.getBounds());
+    // set polyline on map (should wait until route found and should have stored array of the values)
+    // let polyline = L.polyline([[dataStruct.startLat, dataStruct.startLon], [dataStruct.targetLat, dataStruct.targetLon]], {color: 'red'}).addTo(map);
+    // map.fitBounds(polyline.getBounds());
 
     if (userLocation) {
-        console.log(userLocation, dataStruct);
+
+        // below is the logic for finding street and then proper lowest (closest) lat/lon value (where the user should go) (all in relation to having to use the OverpassAPI),
+        // possible if you have any position (but it's likely you would want to use the users location).
         let l = await findStreets(dataStruct.startLat, dataStruct.startLon);
-        let q = await findStreets(l.lon, l.lat);
-        console.log(q);
+        if (l) {
+
+            let p = l.elements;
+            for (const q in p) {
+                let c = p[q];
+                let d = await findNodeValue(c.id, c.nodes);
+                let o = d.elements;
+                for (const g in o) {
+                    let k = o[g];
+                    let newLat = Math.abs(dataStruct.startLat - k.lat);
+                    let newLon = Math.abs(dataStruct.startLon - k.lon);
+                    let dataStructClosest = dataStruct.closest;
+                    if (newLat < dataStructClosest.latVal && newLon < dataStructClosest.lonVal) {
+                        dataStructClosest.latitude = k.lat;
+                        dataStructClosest.longitude = k.lon;
+                        dataStructClosest.latVal = newLat;
+                        dataStructClosest.lonVal = newLon;
+                    }
+
+                }
+            }
+        }
     }
 
     // when lowest returned check if it is within 50 metres of target. if so, stop it.
 }
 
-start();
+start()
+    .then(response => console.log(response))
+    .catch(error => console.error('Route Not Found: ', error));
