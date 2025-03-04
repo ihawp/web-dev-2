5ef2128ea20ef6584017fcdb88ac7141bc72faa9
ihawp <ihawpebay@gmail.com>
2025-03-04 14:57:00 -0600

    faq animation working with the dropdown arrow!

--- warren-p2-site/faq-dropdown.js
+++ warren-p2-site/faq-dropdown.js
@@ -1,8 +1,14 @@
-let open = undefined;
 let items = document.querySelectorAll('details');
+let open = items[0];
+
 
 items.forEach((item) => item.addEventListener('click', (event) => {
-    let faqClicked = event.target;
-    if ( open !== undefined && faqClicked !== open) open.parentElement.open = false;
+    let faqClicked = item.firstElementChild;
+    if (faqClicked !== open) open.parentElement.open = false;
+    faqClicked.lastElementChild.style.transform = 'rotate(90deg)';
+    open.lastElementChild.style.transform = 'rotate(0deg)';
     open = faqClicked;
 }));
+
+
+// find svg and switch to other svg
