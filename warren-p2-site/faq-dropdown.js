3732a762a7271c520d3726523cdb0f8d873d36c7
ihawp <ihawpebay@gmail.com>
2025-03-04 19:47:34 -0600

    js refactored a bit

--- warren-p2-site/faq-dropdown.js
+++ warren-p2-site/faq-dropdown.js
@@ -1,46 +1,60 @@
 let items = document.querySelectorAll('details');
 let open = items[0];
 
-items.forEach((item) => item.addEventListener('click', (event) => {
-    event.preventDefault();
+function updateArrowState(item, openState) {
     let q = item.firstElementChild;
-    if (window.innerWidth < 1128) {
-        if (item.open) {
-            item.open = false;
-            q.firstElementChild.style.color = '#676182';
-            q.lastElementChild.style.transform = 'rotate(0deg)';
-        } else {
-            open.open = false;
-            open.firstElementChild.lastElementChild.style.transform = 'rotate(0deg)';
-            open.firstElementChild.firstElementChild.style.color = '#676182';
-            item.open = true;
-            q.lastElementChild.style.transform = 'rotate(90deg)';
-            q.firstElementChild.style.color = '#d2d0dd';
-        }
-        open = item;
-    }
-}));
+    let arrowIcon = q.lastElementChild;
 
-if (window.innerWidth > 1128) {
-    items.forEach((item) => item.open = true);
+    if (openState) {
+        arrowIcon.style.transform = 'rotate(90deg)';
+        q.firstElementChild.style.color = '#d2d0dd';
+    } else {
+        arrowIcon.style.transform = 'rotate(0deg)';
+        q.firstElementChild.style.color = '#676182';
+    }
 }
 
-window.addEventListener('resize', (event) => {
-    if (event.target.innerWidth > 1128) {
-        items.forEach((item) => {
-            item.open = true
-            item.firstElementChild.firstElementChild.style.color = '#fff';
-        });
-    } else {
-        items.forEach((item) => {
+items.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
 
-            item.open = false;
-            item.firstElementChild.firstElementChild.style.color = '#676182';
-
-        });
-    }
+        // Check window width
+        if (window.innerWidth < 1128) {
+            if (item.open) {
+                updateArrowState(item, false);
+                item.open = false;
+            } else {
+                if (open !== item) {
+                    updateArrowState(open, false);
+                    open.open = false;
+                }
+                updateArrowState(item, true);
+                item.open = true;
+            }
+            open = item;
+        }
+    });
 });
 
 
+function handleResize() {
+    if (window.innerWidth > 1128) {
+        items.forEach((item) => {
+            item.open = true;
+            let q = item.firstElementChild;
+            q.firstElementChild.style.color = '#fff';
+            updateArrowState(item, true);
+        });
+    } else {
+        items.forEach((item) => {
+            item.open = false;
+            let q = item.firstElementChild;
+            q.firstElementChild.style.color = '#676182';
+            updateArrowState(item, false);
+        });
+    }
+}
 
-// find svg and switch to other svg
+window.addEventListener('resize', handleResize);
+
+handleResize();
